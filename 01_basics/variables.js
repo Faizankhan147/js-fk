d465f58e63094bfc9f45d77e273aef1a4cca1554
Faizankhan147 <59408320+Faizankhan147@users.noreply.github.com>
2023-10-18 10:39:27 +0000

    create variables file

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+// const accountId = 9762
+let accountEmail = "faizan.cerp@gmail.com"
+var accountPassword = 9762
+accountCity = "lahore"
+
+
+accountId= 1122                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+accountEmail = "cool_faizan123@hotmail.com"
+accountPassword = 3344
+accountCity = "Karachi"
+
+// console.log(accountId);
+
+console.table([accountId , accountEmail , accountPassword, accountCity])
